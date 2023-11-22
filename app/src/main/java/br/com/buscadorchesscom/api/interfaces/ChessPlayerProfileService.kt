b8b0ae30e8b57ebffb7d2be7e414b21c8c253588
Luanderson Alves <luaoderson@gmail.com>
2023-11-22 02:58:05 -0300

    refactor: changed retrofit to coroutine call

--- app/src/main/java/br/com/buscadorchesscom/api/interfaces/ChessPlayerProfileService.kt
+++ app/src/main/java/br/com/buscadorchesscom/api/interfaces/ChessPlayerProfileService.kt
@@ -1,15 +1,15 @@
 package br.com.buscadorchesscom.api.interfaces
 
 import br.com.buscadorchesscom.model.ChessPlayerProfileData
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ChessPlayerProfileService {
 
     @GET("player/{username}")
-    fun getPlayerProfile(
+    suspend fun getPlayerProfile(
         @Path("username") username: String
-    ): Call<ChessPlayerProfileData>
+    ): Response<ChessPlayerProfileData>
 
 }
--- app/src/main/java/br/com/buscadorchesscom/model/ChessPlayerProfileData.kt
+++ app/src/main/java/br/com/buscadorchesscom/model/ChessPlayerProfileData.kt
@@ -1,5 +1,12 @@
 package br.com.buscadorchesscom.model
 
+import com.google.gson.annotations.SerializedName
+
 data class ChessPlayerProfileData (
-    val avatar: String
+    val avatar: String,
+    val name: String,
+    val joined: Long,
+    val status: String,
+    val title: String,
+    @SerializedName("last_online") val lastOnline: Long,
 )
